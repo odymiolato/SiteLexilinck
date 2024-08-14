d2ef4e913d4f697ee8dfa59a9caaa732a5f0d678
Ody <125280969+odymiolato@users.noreply.github.com>
2024-08-14 09:36:17 -0400

    cambios

--- js/slider.js
+++ js/slider.js
@@ -1,29 +1,27 @@
-let slider = document.querySelector(".slider-contenedor")
-let sliderIndividual = document.querySelectorAll(".contenido-slider")
+let slider = document.querySelector(".slider-contenedor");
+let sliderIndividual = document.querySelectorAll(".contenido-slider");
 let contador = 1;
 let width = sliderIndividual[0].clientWidth;
 let intervalo = 9000;
 
-window.addEventListener("resize", function(){
-    width = sliderIndividual[0].clientWidth;
-})
+window.addEventListener("resize", function () {
+  width = sliderIndividual[0].clientWidth;
+});
 
-setInterval(function(){
-    slides();
-},intervalo);
+setInterval(function () {
+  slides();
+}, intervalo);
 
+function slides() {
+  slider.style.transform = "translate(" + -100 * contador + "%)";
+  slider.style.transition = "transform .9s";
+  contador++;
 
-
-function slides(){
-    slider.style.transform = "translate("+(-1900*contador)+"px)";
-    slider.style.transition = "transform .9s";
-    contador++;
-
-    if(contador == sliderIndividual.length){
-        setTimeout(function(){
-            slider.style.transform = "translate(0px)";
-            slider.style.transition = "transform 0s";
-            contador=1;
-        },1500)
-    }
+  if (contador == sliderIndividual.length) {
+    setTimeout(function () {
+      slider.style.transform = "translate(0px)";
+      slider.style.transition = "transform 1s";
+      contador = 1;
+    }, 1500);
+  }
 }
